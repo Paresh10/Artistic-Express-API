b04565dfac3ce15c95013aace4a6491dd712b1ae
Paresh Sharma <Paresh.Sharma@Valaries-MBP.hsd1.il.comcast.net>
2020-05-16 22:31:20 -0500

    Mongo

--- db/db.js
+++ db/db.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose')
 
 const connectionString = process.env.MONGODB_URI
 
+const MongoClient = require("mongodb").MongoClient;
+
+const client = new MongoClient(connectionString, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const collection = client.db("test").collection("devices");
+  client.close();
+});
+
+
 mongoose.connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
